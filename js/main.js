7ed905c631de608a02c69e021fbbc70fe03bd505
Damo <damonkhan92@gmail.com>
2017-10-10 05:47:53 +1300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Expression
+var fullName = function () {
+    var first = prompt("first name");
+    var last = prompt("last name");
+    return first + " " + last;
+};
+
+var name = fullName();
+
+alert(name);
+
